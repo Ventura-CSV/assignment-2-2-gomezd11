1a80d2139431e450a2ba6e0000408e77e89cd53c
gomezd11 <gomezd1290@gmail.com>
2023-08-25 09:02:15 -0700

    Assigment 2-2

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def main():
     Use the same variables: celcius fahrenheit 
     ##################################################
     """
+    celcius = 23
+
+    fahrenheit = celcius * 9/5 + 32
+
+    print(f"{celcius:.2f} Celsius is equal to {fahrenheit:.2f} Fahrenheit")
 
     """
     ########################################
